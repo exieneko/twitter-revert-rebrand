0c84072a584a8003fe0eddcf8525910f86e9765a
exieneko <exieneko@proton.me>
2024-02-29 20:15:07 +0100

    meow

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# twitter-revert-rebrand
+# Twitter Revert Rebrand
+
+A Chrome extension to revert the Twitter UI to the pre-Musk era
+
+## Installation
+
+Since the extension is not on the Chrome webstore, you can install it by loading it as an unpacked extension
+
+1. Download the [latest release](https://github.com/lesbiancatgirl/twitter-revert-rebrand/releases/latest)
+2. Extract the contents of the zip into any folder
+3. Open your browser's extensions page (`chrome://extensions` or similar)
+4. Enable developer mode, click "load unpacked" and select the folder containing the `manifest.json` file
+
+## Credits
+
+Inspired by [cygaar/old-twitter](https://github.com/cygaar/old-twitter) (no longer maintained)
---
+++ src/content.ts
@@ -0,0 +1,104 @@
+function waitForElement<T extends HTMLElement>(selectors: string[], callback: (element: T) => void) {
+    function doTheThing() {
+        selectors.forEach(selector => {
+            const element = document.querySelector<T>(selector);
+            if (element) {
+                callback(element);
+            }
+        });
+    }
+
+    doTheThing();
+    new MutationObserver(doTheThing).observe(document.body, { childList: true, subtree: true });
+}
+
+function waitForElements<T extends HTMLElement>(selectors: string[], callback: (element: T) => void) {
+    function doTheThing() {
+        selectors.forEach(selector => {
+            const elements = document.querySelectorAll<T>(selector);
+            elements.forEach(element => callback(element));
+        });
+    }
+
+    doTheThing();
+    new MutationObserver(doTheThing).observe(document.body, { childList: true, subtree: true });
+}
+
+function replaceText(element: HTMLElement, findValue: string, replaceValue: string) {
+    if (element.textContent?.includes(findValue)) {
+        element.innerHTML = element.innerHTML?.replace(findValue, replaceValue);
+    }
+}
+
+function replaceImage(element: HTMLElement, imagePath: string, size: number) {
+    if (element.classList.contains('twitter-icon')) {
+        return;
+    }
+
+    fetch(chrome.runtime.getURL(imagePath)).then(response => response.text()).then(text => {
+        const content = new DOMParser().parseFromString(text, 'image/svg+xml');
+        const svg = content.querySelector<HTMLElement>('svg');
+        const path = svg?.querySelector<HTMLElement>('path');
+
+        element.innerHTML = path?.outerHTML || '<path></path>';
+
+        element.setAttribute('viewBox', svg?.getAttribute('viewBox') || '0 0 16 16');
+        element.style.width = `${size}px`;
+        element.style.height = `${size}px`;
+        element.style.fill = document.querySelector<HTMLElement>('[data-testid="SideNav_NewTweet_Button"]')?.style.backgroundColor || '#1d9bf0';
+
+        element.classList.add('twitter-icon');
+    });
+}
+
+
+
+waitForElement<HTMLTitleElement>(['title' /* title */ ], element => {
+    const values = {
+        ' on X': ' on Twitter',
+        ' / X': ' / Twitter',
+        'repost': 'Retweet',
+        'Quotes': 'Quote Tweets',
+        'Post': 'Tweet',
+        'post': 'Tweet'
+    };
+
+    Object.entries(values).forEach(([key, value]) => {
+        replaceText(element, key, value);
+    });
+});
+
+waitForElement<HTMLAnchorElement>(['[rel~="icon"]' /* favicon */ ], element => {
+    element.href = chrome.runtime.getURL('images/twitter-128.png');
+});
+
+waitForElement(['#placeholder > svg' /* loading icon */ ], element => {
+    replaceImage(element, 'images/twitter.svg', 64);
+});
+
+waitForElement(['[aria-label="X"] svg' /* sidebar icon */ ], element => {
+    replaceImage(element, 'images/twitter.svg', 32);
+});
+
+waitForElements(['[data-testid="socialContext"]' /* retweeted text */ ], element => {
+    replaceText(element, 'reposted', 'Retweeted');
+});
+
+waitForElement([
+    '[data-testid="SideNav_NewTweet_Button"] span span span', // sidebar tweet button
+    '[data-testid="toolBar"] > div span span', // inline tweet button
+    '[data-testid="cellInnerDiv"] span', // show new tweets button
+    '[data-testid="pillLabel"] span span span', // show new tweets popup
+    '[data-testid="notification"] span span' // notifications
+], element => {
+    replaceText(element, 'post', 'Tweet');
+    replaceText(element, 'Post', 'Tweet');
+});
+
+waitForElements([
+    '[data-testid="trend"] > div > div:nth-child(3) > span', // trending tweets
+    'h2 span' // heading
+], element => {
+    replaceText(element, 'post', 'Tweet');
+    replaceText(element, 'Post', 'Tweet');
+});
